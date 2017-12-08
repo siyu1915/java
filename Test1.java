b5b1345bdbb35b97922a8779a10114321c61f00f
siyu1915 <siyu1915@gmail.com>
2017-12-08 14:58:18 +0800

    Create Test1.java
    2017 12 08 _ 1

---
+++ Test1.java
@@ -0,0 +1,10 @@
+class Test1 {
+    public static void main(String args[]) {
+	System.out.println("java\n语\b言");
+	System.out.println("java\r语言");
+	System.out.println("java\t语言");
+	System.out.println("\\java语言\\");
+	System.out.println("\'java语言\'");
+	System.out.println("\"java语言\"");
+    }
+}
