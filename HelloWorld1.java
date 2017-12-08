0894c74d3aa4935dea184584c3fcaba3f7ff855f
siyu1915 <siyu1915@gmail.com>
2017-12-08 15:19:25 +0800

    2017/12/08 HelloWorld1.java

---
+++ HelloWorld1.java
@@ -0,0 +1,8 @@
+public class HelloWorld1 {
+    		public static void main(String[] args) {
+        		String message = “Hello World!”;        			myPrint(message);	
+		}
+		private static void myPrint(String s) {
+			System.out.println(s);
+		}
+	}
